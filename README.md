fce873c46c33d0f7478cfee6d17bc77b2837d3a8
Haoyu Zhang <noahzhy@gmail.com>
2023-11-23 23:48:25 +0000

    Update loss functions and add usage example

--- README.md
+++ README.md
@@ -5,11 +5,32 @@ Various loss function in tf, some of them were implemented by official tensorflo
 
 ## Supported Loss Functions
 
-### CTC
+### Recognition
 
 - [x] CTC Loss
 - [x] Focal CTC Loss
-- [ ] CTC Center Loss
+- [x] CTC Center Loss
+   - [x] pytorch version from [here](./torch_losses.py)
+
+#### CTC Center Loss comparison
+
+```python
+from unit_test import test_ctc_center_loss
+
+n_class = 100
+dims = 128
+x = np.random.normal(size=(32, 16, dims)).astype(np.float32)
+labels = np.random.randint(0, n_class, (32, 16)).astype(np.int32)
+test_ctc_center_loss(x, labels, n_class, dims)
+```
+
+results:
+
+```bash
+torch loss: 127.31403
+tf    loss: 127.314026
+loss  diff: 7.6293945e-06
+```
 
 ### Classification
 
@@ -26,3 +47,16 @@ Various loss function in tf, some of them were implemented by official tensorflo
 - [x] Dice BCE Loss
 - [x] Dice Loss
 - [x] IoU Loss
+
+## Usage
+
+```python
+from losses import *
+
+# e.g. CTC Loss
+loss = CTCLoss()
+x = tf.random.normal([2, 10, 20])
+y = tf.random.uniform([2, 10], maxval=20, dtype=tf.int32)
+out = loss(x, y)
+print(out)
+```
--- unit_test.py
+++ unit_test.py
@@ -9,32 +9,32 @@ import torch
 import torch.nn as nn
 
 from losses import CTCCenterLoss
-from torch_test import CenterLoss
+from torch_losses import CenterLoss
 
 
 # test ctccenterloss between torch and tf
-def test_ctccenterloss(pred, target):
-    dims = 96
+def test_ctc_center_loss(pred, target, n_class=100, dims=96):
     # torch
     x = torch.from_numpy(pred).reshape(32 * 16, dims)
     labels = torch.from_numpy(target).reshape(32 * 16, )
-    torch_loss = CenterLoss(num_classes=86, feat_dim=dims, random_init=False, use_gpu=False)(x, labels)
+    torch_loss = CenterLoss(num_classes=n_class, feat_dim=dims, random_init=False, use_gpu=False)(x, labels)
     # to numpy float32
     torch_loss = torch_loss.detach().numpy().astype(np.float32)
 
     # tf
-    tf_loss = CTCCenterLoss(num_classes=86, feat_dims=dims, random_init=False)(pred, target)
+    tf_loss = CTCCenterLoss(num_classes=n_class, feat_dims=dims, random_init=False)(pred, target)
     # to numpy float32
     tf_loss = tf_loss.numpy().astype(np.float32)
 
     # compare
     print("torch loss:", torch_loss)
-    print("tf loss:", tf_loss)
-    print("loss diff:", torch_loss - tf_loss)
+    print("tf    loss:", tf_loss)
+    print("loss  diff:", torch_loss - tf_loss)
 
 
 if __name__ == '__main__':
-    # load from npy file
-    x = np.load("features.npy")
-    labels = np.load("labels.npy")
-    test_ctccenterloss(x, labels)
+    n_class = 100
+    dims = 128
+    x = np.random.normal(size=(32, 16, dims)).astype(np.float32)
+    labels = np.random.randint(0, n_class, (32, 16)).astype(np.int32)
+    test_ctc_center_loss(x, labels, n_class, dims)
